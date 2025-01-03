7dae99423b538ace7f7cd8cb0a82250af9202c88
leuuh <leonardodealmeidagomes83@gmail.com>
2025-01-03 00:32:23 -0300

    testes utilizando massa de dados

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -13,17 +13,30 @@ describe('Funcionalidade: Produtos', () => {
             
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto = 'Rocco Gym Tank'
         produtosPage.buscarProdutos(produto)
         cy.get('.product_title').should('contain', produto)
     });
 
-    it('Deve visitar a pagina do produto', () => {
-        
-    });
-
     it('Deve adicionar produto ao carrinho', () => {
-        
-    });
+        let qtd = 5;
+        produtosPage.buscarProdutos('Atomic Endurance Running Tee (Crew-Neck)');
+        produtosPage.addProdutoCarrinho('L', 'Black', qtd);
+        cy.get('.woocommerce-message').should('include.text', `${qtd} × “Atomic Endurance Running Tee (Crew-Neck)” foram adicionados no seu carrinho.`)
+      });
+
+      it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados =>{
+
+            produtosPage.buscarProdutos(dados[1].nomeProduto);
+            produtosPage.addProdutoCarrinho(
+                dados[1].tamanho,
+                dados[1].cor,
+                dados[1].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+          });
+        })
+
+
 });
